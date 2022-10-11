642b1f15dfdc66a264e4f73ab0cb0dc3f08204ad
Jorge Romero <jorgeromero@Jorges-MacBook-Pro.local>
2022-10-11 15:24:24 -0400

    added show cart modal logic to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
-  return (
-    <CartProvider>
-      <Header />
+
+  const [modal,setModal] = useState(false)
+  const showModal=()=>{setModal(true)}
+  const hideModal=()=>{setModal(false)}
+  return (  <CartProvider>
+        {modal && <div>gangeeeeee</div>}
+      <Header onShowCart={showModal} />
       <main>
         <Meals />
       </main>
